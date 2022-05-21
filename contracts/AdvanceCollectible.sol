51755af11536189c36378560759dfa1efb285d62
j4k3ng <41842937+j4k3ng@users.noreply.github.com>
2022-05-21 15:57:06 +0100

    Advance NFT

--- contracts/AdvanceCollectible.sol
+++ contracts/AdvanceCollectible.sol
@@ -2,9 +2,24 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SimpleCollectible is ERC721{
+contract AdvanceCollectible is ERC721{
     uint256 public tokenCounter;
      
     constructor() public ERC721 ("Moga", "OGA"){
         tokenCounter = 0;
+        
     }
+
+    function createCollectible() public returns(uint256){
+        uint256 newTokenId = tokenCounter;  // define newTokenId equal to the current tokenCounter
+        _safeMint(msg.sender, newTokenId);  // mint the nft from the address of the sender of the contract with the tokenid just defined
+        tokenCounter = tokenCounter + 1;    // incremenat the token counter for the next mint
+        return newTokenId;
+    }
+
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not owner or not approved");
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+}
